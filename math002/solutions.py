1c78f2044f97fb67fd489919d33c5578a3f9dd92
Anuradha <anuradha.shinde@enine.school>
2024-04-16 16:41:01 +0530

    This is the solution for fibonacci series

--- math002/solutions.py
+++ math002/solutions.py
@@ -16,10 +16,10 @@ def solver(start, end, even=False,odd=False):
         if nxt_val > end:
             break
         if nxt_val >= start:
-            pass
+            print(nxt_val)
         fibs.append(nxt_val)
     even=[i for i in fibs if i%2==0 and i>=start]
     odd=[i for i in fibs if i%2!=0 and i>=start]
     return f'Even :{even}, Odd:{odd}'
 
-print(solver(100,600))
+print(solver(100,800))
