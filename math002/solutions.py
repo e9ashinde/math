12c2c5e8fd36b1ec8c1637dbbfcb092c4c4109da
Anuradha <anuradha.shinde@enine.school>
2024-04-16 16:32:46 +0530

    This is the solution file for fibonacci series

---
+++ math002/solutions.py
@@ -0,0 +1,25 @@
+def answer():
+   a,b=1,1
+   total = 0
+   while a <= 4000000:
+      if a % 2 == 0:
+        total += a
+      a, b = b, a+b 
+   return total
+
+print(answer())
+
+def solver(start, end, even=False,odd=False):
+    fibs= [0, 1]
+    while True:
+        nxt_val = fibs[-1] + fibs[-2]
+        if nxt_val > end:
+            break
+        if nxt_val >= start:
+            pass
+        fibs.append(nxt_val)
+    even=[i for i in fibs if i%2==0 and i>=start]
+    odd=[i for i in fibs if i%2!=0 and i>=start]
+    return f'Even :{even}, Odd:{odd}'
+
+print(solver(100,600))
